423cdeea3370b9d377d627a03b1ffbc03fcfd9af
navnit0707 <knavnit81@gmail.com>
2023-04-07 20:38:00 +0530

    setup of env file  and keeping it seperate in src/config

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,12 +1,11 @@
 //common js moduling
 const express = require("express");
-require("dotenv").config();
+const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   // create the express object
   const app = express();
 
-  const PORT = 3000;
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
   });
