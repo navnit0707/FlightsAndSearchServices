036e33a3ad664c9a0e1a4e0f1f765f8517c905e3
navnit0707 <knavnit81@gmail.com>
2023-04-09 07:38:59 +0530

    Added city controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,111 @@
+const { CityService } = require("../services/index");
+
+const cityService = new CityService();
+
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Successfully Created a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create a city",
+      err: err,
+    });
+  }
+};
+
+// DELETE -> /city/:id
+const destroy = async (req, res) => {
+  try {
+    const response = await cityService.deleteCity(req.params.id);
+
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully deleted a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to delete a city",
+      err: err,
+    });
+  }
+};
+
+// GET-> /city/:id
+const get = async (req, res) => {
+  try {
+    const response = await cityService.getCity(req.params.id);
+
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully fetched a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get the city",
+      err: err,
+    });
+  }
+};
+
+//Patch -> /city/:id
+const update = async (req, res) => {
+  try {
+    const response = await cityService.updateCity(req.params.id, req.body);
+
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully updated a city",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update a city",
+      err: err,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  destroy,
+  update,
+  get,
+};
+
+/*
+ *  data Flow :-
+ *  - controller -> service  to apply
+ *  - business logic-> call repositry to intract db
+ * The use cses of controller is request comes to controller,
+ *  then controller  pass all the req data to serice & repositorylayer,
+ *  they will process somehow and return to controller , controller will form
+ * res structure and send it to user
+ */
